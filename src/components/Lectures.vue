45ba6b506ac7ebf6a0a7dd6c53690fa80ab7fc03
Lourdes Aguirre <loumaagui@gmail.com>
2021-10-29 01:17:02 -0600

    vistas lectura hecha

--- src/components/Lectures.vue
+++ src/components/Lectures.vue
@@ -1,13 +1,67 @@
 <template>
-  <h2>Lectures Works</h2>
+  <main class="libros">
+    <section class="libros__info">
+      <h2 class="libros__info-title">Vive una experiencia de lectura increible</h2>
+      <h3 class="libros__info-subtitle">
+        Encuentra los mejores libros educativos
+      </h3>
+    </section>
+
+    <section class="libros__carrousel">
+      <Carrousel :items="libros" />
+    </section>
+  </main>
 </template>
 
 <script>
+import Carrousel from "./Carrousel";
+
 export default {
-  name: "Lectures",
+  name: "Libros",
+  components: { Carrousel },
+  mounted() {
+    this.libros = [
+      "item1",
+      "item2",
+      "item3",
+      "item4",
+      "item5",
+      "item6",
+      "item7",
+    ];
+  },
   data: () => ({
-    //
+    libros: [],
   }),
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.libros {
+  background-color: #242528;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+
+  &__info {
+    display: flex;
+    margin-top: 2em;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    &-title {
+      font-size: 2em;
+      font-weight: 800;
+    }
+    &-subtitle {
+      color: grey;
+    }
+  }
+  &__carrousel {
+    height: 12em;
+    width: 60%;
+    margin-top: 2em;
+  }
+}
+</style>
+
